3d291ef971f2211d74e8900927b34c8e4563586b
specnazm <laketic.milena98@gmail.com>
2021-07-14 00:31:49 +0200

    added model

--- parser/main.py
+++ parser/main.py
@@ -1,56 +1,32 @@
-from textx import metamodel_from_file, TextXSyntaxError
-from textx.export import model_export
-from os import listdir
-from os.path import isfile, join
-from subprocess import check_call
-
-
-
-def print_stm(stm):
-    if (type(stm).__name__ == "AssignmentExpression"):
-        print("AssignmentExpression ", stm.var, ":=", stm.value)
-    elif (type(stm).__name__ == "UnaryMessage"):
-        print("UnaryMessage Poruka : ", stm.selector, " se salje objektu : ", stm.receiver)
-    else:
-        print(stm)
+import os
+from fnmatch import fnmatch
+from textx import metamodel_from_file
+from model import Document, Workspace
+from util import calculate_time
 
 
 def find_unary_msg(model):
     pass
 
+@calculate_time
+def load_workspace(root, meta_model):
+    workspace = Workspace(root, meta_model)
+    pattern = "*.st"
+    for path, subdirs, files in os.walk(root):
+        for name in files:
+            if fnmatch(name, pattern):
+                file_name = os.path.join(path, name)
+                doc = Document(file_name)
+                workspace.add_document(doc)
+
+    workspace.parse_all()
+
+    return workspace
 
 def start():
-    file_names = [f for f in listdir("examples") if isfile(join("examples", f))]
-    failed_num = 0
-
-
+    root = 'examples\\'
     meta_model = metamodel_from_file("grammar/pharo.tx")
-    for file_name in file_names:
-        try:
-            model = meta_model.model_from_file("examples/" + file_name)
-
-            find_unary_msg(model)
-            dot_file_name = 'dot/' + file_name.replace(".st", "") +'.dot'
-            model_export(model, dot_file_name)
-            check_call(['dot','-Tpng',dot_file_name,'-o','images/' + file_name.replace(".st", "") + '.png'])
-
-            #
-            # print("========= SUCCESSFULLY PARSED ========")
-            # print("FILE NAME : ", file_name)
-            # print()
-
-        except TextXSyntaxError as err:
-            print ("========= TEST FAILED ========")
-            print ("FILE NAME : ", file_name)
-            print (err)
-            print()
-            failed_num += 1
-
-
-    print()
-    print("TEST FAILED : ", failed_num)
-    print("TEST PASSED : ", len(file_names) - failed_num)
-
+    workspace = load_workspace(root, meta_model)
 
 if __name__ == '__main__':
     start()
---
+++ parser/model.py
@@ -0,0 +1,81 @@
+from textx import TextXSyntaxError
+from util import *
+
+
+class Workspace(object):
+    def __init__(self, root_uri, meta_model):
+        self.__root_uri = root_uri
+        self._docs = {}
+        self.__meta_model = meta_model
+
+    @property
+    def meta_model(self):
+        return self.__meta_model
+
+    @property
+    def root_uri(self):
+        return self.__root_uri
+
+    @property
+    def documents(self):
+        return self._docs
+
+    def get_document(self, doc_uri):
+        try:
+            return self._docs[doc_uri]
+        except KeyError:
+            return None
+
+    def add_document(self, document, content=None):
+        self._docs[document.uri] = document
+
+    @calculate_time
+    def parse_all(self):
+        for doc in self.documents.values():
+            doc.parse_model(self.__meta_model)
+
+
+
+class Document(object):
+    def __init__(self, uri, source=None):
+        self.__uri = uri
+        self.__source = source
+        self.__errors = []
+        self.__model = None
+
+    @property
+    def uri(self):
+        return self.__uri
+
+    @property
+    def source(self):
+        if self._source is None:
+            with open(self.uri, 'r') as f:
+                self.__source = f.read()
+        return self.__source
+
+    @property
+    def model(self):
+        return self.__model
+
+    @write_errors
+    @convert_to_png
+    def parse_model(self, meta_model):
+        try:
+            model = meta_model.model_from_file(self.__uri)
+            self.__model = model
+
+        except TextXSyntaxError as err:
+            print("Syntax error in file : ", self.__uri)
+            self.__errors.append(err)
+
+        return self.__errors, self.__uri, self.__model
+
+    @property
+    def is_valid_model(self):
+        return len(list(self.__errors)) == 0
+
+    @property
+    def errors(self):
+        return self.__errors
+
